f44887636eb9dad405a8a2aad715352da5472be3
Dhaval <dhvl.ptl123@yahoo.in>
2022-05-15 07:21:27 +0530

    test: test description change

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { expect, it } from 'vitest'
 import { render, screen } from './utils/testUtil'
 import App from './App'
 
-it('Should render title', async () => {
+it('Should render title correctly', async () => {
   render(<App />)
   expect(screen.getByText('Vite with React')).toBeDefined()
 })
